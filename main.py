ea2ada751b22c4441dcac8fafc0dfc453992bcda
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2021-12-24 00:45:59 +0300

    Прописал основные методы api

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from werkzeug.wrappers import response
 import json
 import os
 
-
-
+from database_controller import DatabaseController
+from error import ApiErrors
 
 app = Flask(__name__, static_folder='../photodatabase/build/web')
 
@@ -18,109 +18,111 @@ def static_file(path):
     return app.send_static_file(path)
 
 
+@app.route('/api/folder', methods=['POST'])
+def postFolder():
+    title = request.form.get('title')
+    description = request.form.get('description')
+    if title.isspace:
+        return ApiErrors.badArgumentsError.jsonify()
+    return jsonify(db.folders.create(title=title, description=description))
+
+
+@app.route('/api/folder/<id>', methods=['PATCH'])
+def patchFolder(id):
+    title = request.form.get('title')
+    description = request.form.get('description')
+    if not id.isnumeric:
+        return ApiErrors.badArgumentsError.jsonify()
+    return jsonify(db.folders.edit(id=id, title=title, description=description))
+
+
+@app.route('/api/folder/<id>', methods=['DELETE'])
+def deleteFolder(id):
+    if not id.isnumeric:
+        return ApiErrors.badArgumentsError.jsonify()
+    db.folders.delete(id=id)
+    return 
+
+@app.route('/api/folder/<id>', methods=['GET'])
+def getFolder(id):
+    if not id.isnumeric:
+        return ApiErrors.badArgumentsError.jsonify()
+    return jsonify(db.folders.get(id=id))
+
+@app.route('/api/folder/', methods=['GET'])
+def getFolders():
+    return jsonify(db.folders.getAll())
+
+@app.route('/api/image/', methods=['GET'])
+def getImage():
+    return jsonify(db.image.getAll())
+
+@app.route('/api/image/<id>', methods=['GET'])
+def getImages(id):
+    return jsonify(db.image.get(id=id))
+
+@app.route('/api/image', methods=['POST'])
+def postImage():
+    image = request.files['file']
+    title = request.form.get('title')
+    description = request.form.get('description')
+    folder_id = request.form.get('folder_id')
+    if image.content_type.split('/')[0] == "image":
+        return ApiErrors.badFileType.jsonify()
+    filename = secure_filename(image.filename)
+    path = "photos/" + filename
+    image.save(path)
+    create_image = db.images.create(title=title, description=description, path=path)
+    if folder_id != 'null':
+        db.linkImage(create_image['id'], folder_id)
+    return jsonify(create_image)
+
+@app.route('/api/image/<id>', methods=['PATCH'])
+def patchImage(id):
+    title = request.form.get('title')
+    description = request.form.get('description')
+    return jsonify(db.images.edit(id=id,title=title, description=description))
+
+@app.route('/api/image/<id>', methods=['DELETE'])
+def deleteImage(id):
+    db.images.delete(id=id)
+    return
+
+@app.route('/api/image/<id>/link/<folder>', methods=['POST'])
+def postLink(id, folder):
+    return jsonify(db.images.addToFolder(image_id=id, folder_id=folder))
+
+@app.route('/api/image/<id>/link/<folder>', methods=['DELETE'])
+def deleteLink(id, folder):
+    return jsonify(db.images.removeFromFolder(image_id=id, folder_id=folder))
+
+
+@app.route('/api/union', methods=['GET'])
+def getUnion():
+    return jsonify(db.getUnion())
+
+
 def main():
     app.config.from_file("config.json", load=json.load)
     __dbpassword = app.config.get("DBPASSWORD")
     if not __dbpassword:
         raise ValueError("Not found DBPASSWORD in config.json. Check README.MD!")
+    config = {
+        'host': 'db-learning.ithub.ru',
+        'user': '2p1s16',
+        'password':__dbpassword,
+        'database':'2p1s16',
+        'autocommit':True,
+        'charset':'utf8mb4',
+    }
+    global db 
+    db = DatabaseController(config)
+    app.run(
+        debug=False, 
+        port=1117,
+        # host='db-learning.ithub.ru'
+    )
+    del db
 
 if __name__ == "__main__":
     main()
-
-
-# @app.route('/api/getFolders', methods=['GET'])
-# def getFolders():
-#     return jsonify(db.getFolders())
-
-# @app.route('/api/createFolder', methods=['POST'])
-# def createFolderAPI():
-#     title = request.form.get('title')
-#     description = request.form.get('description')
-#     return jsonify(db.createFolder(title=title, description=description))
-
-# @app.route('/api/getFolderById/<id>', methods=['GET'])
-# def getFolderByIdAPI(id):
-#     return jsonify(db.getFolderById(id=id))
-
-# @app.route('/api/deleteFolder/', methods=['DELETE'])
-# def removeFolderAPI():
-#     id = request.form.get('id')
-#     return jsonify(db.removeFolder(id))
-
-# @app.route('/api/uploadImage', methods=['POST'])
-# def uploadImageAPI():
-#     file = request.files['file']
-#     title = request.form.get('title')
-#     description = request.form.get('description')
-#     folder = request.form.get('folder_id')
-#     filename = secure_filename(file.filename)
-#     path = "photos/" + filename
-#     file.save(path)
-#     create_image = db.createImage(title=title, description=description, path=path)
-#     if (folder != 'null'):
-#         db.linkImage(create_image['id'], folder)
-#     return jsonify(create_image)
-
-# @app.route('/api/getImages', methods=['GET'])
-# @app.route('/api/getImages.html', methods=['GET'])
-# def getImagesAPI():
-#     return jsonify(db.getImages())
-
-# @app.route('/api/images/<id>', methods=['GET'])
-# def showImageAPI(id):
-#     return send_file(db.getImageById(id=id)['path'])
-
-# @app.route('/api/getUnion', methods=['GET'])
-# def getUnionAPI():
-#     return jsonify(db.getUnion())
-
-
-
-# @app.route('/getImagesByFolderId/<id>', methods=['GET'])
-# def getImagesByFolderId(id):
-#     return jsonify(db.getImagesByFolderId(folder_id=id))
-
-# @app.route('/getLinkImage/<id>', methods=['GET'])
-# def getLinkImageFromId(id):
-#     return jsonify(db.getLinkFromId(link_id=id))
-
-# @app.route('/getLinkImage', methods=['GET'])
-# def getLinkImage():
-#     image_id = request.form.get('image_id')
-#     folder_id = request.form.get('folder_id')
-#     return jsonify(db.getLinkImage(image_id=image_id, folder_id=folder_id))
-
-
-
-    
-# @app.route('/linkImage', methods=['POST'])
-# def linkImage():
-#     image_id = request.form.get('image_id')
-#     folder_id = request.form.get('folder_id')
-#     return jsonify(db.linkImages(image_id=image_id, folder_id=folder_id))
-
-# @app.route('/removeLinkImage', methods=['DELETE'])
-# def removeLinkImage():
-#     id = request.form.get('id')
-#     return jsonify(db.removeLinkImage(id=id))
-
-# @app.route('/removeImage', methods=['POST'])
-# def removeImage():
-#     id = request.form.get('id')
-#     return jsonify(db.removeLinkImage(id=id))
-    
-
-
-# if __name__ == '__main__':
-#     global db 
-#     config = {
-#         'host': 'db-learning.ithub.ru',
-#         'user': '2p1s16',
-#         'password':'134-285-256',
-#         'database':'2p1s16',
-#         'autocommit':True,
-#         'charset':'utf8mb4',
-#     }
-    # db = DatabaseController(config)
-
-    # app.run(debug=False, port=1116,host='db-learning.ithub.ru')
